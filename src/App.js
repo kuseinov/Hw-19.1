534227991c7486212d96e6f16cc9b055a218039b
Abai <abai.kuseinov.kg@gmail.com>
2022-11-16 13:57:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Items from "./Items";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todo, setTodo] = useState([]);
+  
+  function addTodo(event) {
+    setValue(event.target.value);
+  }
+
+  const addTodoPost = async () => {
+    await fetch("https://fake-api-backend.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify({ 'name': value }),
+      headers: { "Content-Type": "application/json" },
+    });
+    addTodoGed()
+  };
+
+  async function addTodoGed() {
+    const response = await fetch(
+      "https://fake-api-backend.herokuapp.com/users"
+    );
+    const data = await response.json();
+    console.log(data);
+    const apdaitedData = data.filter((item) => {
+     return {"name":item.name, "id":item.id}
+    })
+    
+    setTodo(apdaitedData);
+  }
+
+  useEffect(() => {
+    addTodoGed();
+  }, []);
+
+  async function deleteId(id) {
+    await fetch(
+      `https://fake-api-backend.herokuapp.com/users/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    addTodoGed();
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    setTodo((prev) => [...prev, { "name": value }]);
+
+    addTodoPost();
+
+    setValue("");
+    console.log(todo);
+  }
+  const putHandler = async (putId, tast,addChandge) => {
+    console.log(putId, "put id");
+    deleteId(putId);
+    const response = await fetch(
+      `https://fake-api-backend.herokuapp.com/users`,
+      {
+        method: "PUT",
+        body: JSON.stringify({ "name": tast }),
+      }
+    );
+    console.log(response);
+    // addTodoGed()
+  addChandge()
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="App" onSubmit={submitHandler}>
+        <input value={value} type="text" onChange={addTodo} />
+        <button>Добавить</button>
+      </form>
+      <div>
+        <ul>
+          {todo.map((el, index) => (
+            <li key={index}>
+              <Items
+
+                id={el.id}
+                name={el.name}
+                deleteId={deleteId}
+                getData={addTodoGed}
+                editData={putHandler}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
